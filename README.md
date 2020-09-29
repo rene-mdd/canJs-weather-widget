e25b26208bb15d07d250295d8bfd2f10ab46080e
Rene Rodriguez <rene.rrrg@gmail.com>
2020-09-29 13:48:55 +0200

    app ready

---
+++ README.md
@@ -0,0 +1,8 @@
+## Weather location widget
+
+This widget shows the weather forecast base on the coordenates in the map.
+With every click on the map you will get the one-week weather forecast of that specific location.
+
+Technologies: HTML / CSS / canJs
+
+Run: just open the html index with the browser.
--- index.js
+++ index.js
@@ -3,28 +3,33 @@ import { param, StacheElement, type } from "//unpkg.com/can@6/core.mjs";
 
 class GoogleMapView extends StacheElement {
     static view = `
+    <div class='main-div' on:click="this.toggle()">
+    <div this:to="this.mapElement" class="gmap" ></div>
+  {{# if (message)}}
+    <div class="show-modal">
+        <div class="modal-content">
+  
     <div class="weather-widget">
-        <div class="location-entry">
-            <label for="location">Enter your location:</label>
-            <input id="location" type="text" value:to="this.location" />
-        </div>
-
         {{# if(this.forecastPromise.isPending) }}
             <p class="loading-message">Loading forecast…</p>
         {{/ if }}
-
+        
         {{# if(this.forecastPromise.isResolved) }}
+        
             <div class="forecast">
-                <h1>5-Day {{ this.location }} Weather Forecast</h1>
+                <h1>5-Day  Weather Forecast</h1>
                 <ul>
                     {{# for(forecast of this.forecastPromise.value) }}
+                   
                         <li>
                             <span class="date">
                                 {{# if(this.isToday(forecast.date)) }}
+                                
                                     Today
                                 {{ else }}
                                     {{ forecast.date.toLocaleDateString() }}
                                 {{/ if }}
+                               
                             </span>
                             <span class="description {{ this.toClassName(forecast.text) }}">{{ forecast.text }}</span>
                             <span class="high-temp">{{ forecast.high }}<sup>°</sup></span>
@@ -35,17 +40,28 @@ class GoogleMapView extends StacheElement {
             </div>
         {{/ if }}
     </div>
-    <div this:to="this.mapElement" class="gmap"></div>
+    </div>
+    {{/ if}}
+  </div>
+    </div>
+  {{console.log(message)}}
+
 `;
 
 static props = {
-    map: type.Any,
-    mapElement: type.maybeConvert(HTMLElement),
-    vehicles: type.Any,
-    markers: type.Any,
-    location: String,
-    forecastPromise: type.Any
+  
+  map: type.Any,
+  mapElement: type.maybeConvert(HTMLElement),
+  vehicles: type.Any,
+  markers: type.Any,
+  location: String,
+  forecastPromise: type.Any,
+  message: false
   };
+
+  toggle() {
+    this.message = !this.message
+  }
 
   connected() {
     googleAPI.then(() => {
@@ -55,13 +71,15 @@ static props = {
           lat: 52.520008,
           lng: 13.404954
         }
-      });
+      }),
+      
       
       this.map.addListener('click', function(evt) {
         const lat = evt.latLng.lat();
         const lng = evt.latLng.lng();
         console.log(lat, lng);
         // call forecastPromise;
+      
         this.forecastPromise = fetch(
           "https://api.openweathermap.org/data/2.5/forecast?" +
           param({
@@ -73,10 +91,10 @@ static props = {
           })
         ).then(response => {
           return response.json();
-        }).then(transformData);
-      });
+        }).then(transformData)
+      }.bind(this));
     });
-  }
+}
   
   isToday(date) {
     const today = new Date();
@@ -89,25 +107,4 @@ static props = {
 }
 
 customElements.define("google-map-view", GoogleMapView);
-
-class WeatherTracker extends StacheElement {
-  static view = `
-    <div class="top">
-      <div class="header">
-        <h1>{{this.title}}</h1>
-      </div>
-    </div>
-    <div class="bottom">
-      <google-map-view class="map-class"/>
-    </div>
-  `;
-  
-  static props = {
-    title: {
-      default: "Weather Tracker"
-    }
-  };
-}
-
-customElements.define("weather-tracker", WeatherTracker);
 
